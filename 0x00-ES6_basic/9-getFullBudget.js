a832fb18fa6e8e4f89ec4857221efbeefe4b46a9
busayo524 <olowookerebusayo66@gmail.com>
2024-10-03 11:25:54 +0000

    fix: implement getFullBudget function using ES6 method properties

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,16 +1,11 @@
-import getBudgetObject from './7-getBudgetObject.js';
-
-export default function getFullBudgetObject(income, gdp, capita) {
-  const budget = getBudgetObject(income, gdp, capita);
-  const fullBudget = {
-    ...budget,
-    getIncomeInDollars(income) {
-      return `$${income}`;
-    },
-    getIncomeInEuros(income) {
-      return `${income} euros`;
-    },
+export default function getFullBudget() {
+  const budget = {
+    income: 0,
+    expenses: 0,
+    getFullBudget() {
+      return this.income - this.expenses;
+    }
   };
 
-  return fullBudget;
+  return budget;
 }
