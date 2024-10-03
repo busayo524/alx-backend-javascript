29758591c169a6d90a05c016b331bf8c8ec4bf43
busayo524 <olowookerebusayo66@gmail.com>
2024-10-03 10:59:42 +0000

    fix: correct block scoping behavior in taskBlock function

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,17 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  const task = false; // outer scope variable
+  const task2 = true; // outer scope variable
 
+  // Check the input parameter
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
+    // If trueOrFalse is true, redefine task and task2 within this block
+    let task = true;   // inner scope variable
+    let task2 = false; // inner scope variable
+    
+    // Return the inner scope variables
+    return [task, task2]; 
   }
 
-  return [task, task2];  // The outer block's task and task2 will be returned
+  // If falseOrTrue is false, return the outer scope variables
+  return [task, task2]; 
 }
