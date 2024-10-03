f9233b637d5ac0919c2834604d54e331c3c49e1f
busayo524 <olowookerebusayo66@gmail.com>
2024-10-03 13:39:55 +0000

    DEBUGGING ISSUES

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,13 +1,11 @@
-// 1-block-scoped.js
-
 export default function taskBlock(trueOrFalse) {
-  let task = false;  // Outer block-scoped variable
-  let task2 = true;  // Outer block-scoped variable
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-    let task = true;   // Inner block-scoped variable
-    let task2 = false; // Inner block-scoped variable
+    let task = true;
+    let task2 = false;
   }
 
-  return [task, task2]; // Return outer block-scoped variables
+  return [task, task2];
 }
