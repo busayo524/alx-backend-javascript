3a40042d1cb5f7a595b12e2af35ea76b22e215e4
busayo524 <olowookerebusayo66@gmail.com>
2024-10-03 14:26:25 +0000

    Debugging issues

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,10 @@ export default function taskBlock(trueOrFalse) {
   var task2 = true;
 
   if (trueOrFalse) {
-    task = true;
-    task2 = false;
+    // eslint-disable-next-line
+    var innerTask = true;
+    // eslint-disable-next-line
+    var innerTask2 = false;
   }
 
   return [task, task2];
