41d14caa990dd54a6c6eaa734cb7055482de3337
busayo524 <olowookerebusayo66@gmail.com>
2024-10-03 13:28:40 +0000

    1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,13 @@
+// 1-block-scoped.js
+
 export default function taskBlock(trueOrFalse) {
-  let task = false;  // Use let for block scoping
-  let task2 = true;  // Use let for block scoping
+  let task = false;  // Outer block-scoped variable
+  let task2 = true;  // Outer block-scoped variable
 
   if (trueOrFalse) {
-	const task = true;
-	const task2 = false;
+    let task = true;   // Inner block-scoped variable
+    let task2 = false; // Inner block-scoped variable
   }
-return [task, task2]; // Returns outer scope variables
+
+  return [task, task2]; // Return outer block-scoped variables
 }
